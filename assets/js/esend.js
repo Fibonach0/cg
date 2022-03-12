4d99b6720ab81bb14da515a2190b391cc5187198
fibonach0 <ignacio.antu@gmail.com>
2022-03-12 17:49:07 -0300

    No more index

---
+++ assets/js/esend.js
@@ -0,0 +1,25 @@
+function sendEmail() {
+
+    let name = valueById('name')
+    let email = valueById('email')
+    let subject = valueById('subject')
+    let text = valueById('comments')
+
+    Email.send({
+            Host: "smtp.gmail.com",
+            Username: "ignacio.antu@gmail.com",
+            Password: "fqgowavhdwqkopuk",
+            To: 'ignacio.antu@gmail.com',
+            From: email,
+            Subject: subject,
+            Body: " Nombre: " + name + "<br>" +
+                "Consulta: " + text,
+        })
+        .then(function(message) {
+            alert("El mensaje se envió correctamente. Muchas gracias! ")
+        });
+}
+
+function valueById(element) {
+    return document.getElementById(element).value
+}
